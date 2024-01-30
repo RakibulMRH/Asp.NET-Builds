386cadd9bbdc62a7ceadb59c46962cbec80934c3
MD. RAKIBUL HASAN <rakibulmrh4bd@gmail.com>
2024-01-30 23:05:51 +0600

    Class 2 Assignment

---
+++ Class2Assignment/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Class2Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Your Education page.";
+
+            return View();
+        }
+        public ActionResult Personal()
+        {
+            ViewBag.Message = "Your Personal info page.";
+
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Your Projects page.";
+
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            ViewBag.Message = "Your Reference page.";
+
+            return View();
+        }
+    }
+}
